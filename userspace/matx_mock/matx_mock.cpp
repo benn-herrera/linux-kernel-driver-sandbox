f439ebfb51a0005f76a73c6c47e711ba90220d2a
Benn Herrera <benn.herrera@gmail.com>
2026-09-22 19:26:32 -0700

    userspace test program working. dev loop closed. now to iterate.

--- userspace/matx_mock/matx_mock.cpp
+++ userspace/matx_mock/matx_mock.cpp
@@ -3,18 +3,44 @@
  * userspace test of matx_mock driver ABI
  */
 
-#include <stdio.h>
-#include <sys/utsname.h>
+#include <unistd.h>
+#include <iostream>
+#include <cstring>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/errno.h>
+
 #include <matx_mock/mxm_ioctl.h>
 
-int main(void)
-{
-	struct utsname u;
+struct AutoFD {
+  int fd = -1;
+  AutoFD(int fd) : fd(fd) {}
+  operator int() const { return fd; }
+  ~AutoFD() {
+    if (fd >= 0) {
+      close(fd);
+      fd = -1;
+    }
+  }
+};
 
-	if (uname(&u) != 0) {
-		perror("uname");
+int main(void) {
+	AutoFD fd = open("/dev/matx_mock", O_RDWR|O_SYNC);
+	if (fd < 0) {
+	  std::cerr << "failed opening device: " << strerror(errno) << std::endl;
+	  return 1;
+	}
+
+	mxm_info mxm;
+
+	if (ioctl(fd, MXM_IOC_INFO, long(&mxm)) < 0) {
+	  std::cerr << "ioctl failed getting device info: " << strerror(errno) << std::endl;
 		return 1;
 	}
-	printf("matx_mock: userspace ok on %s\n", u.release);
+
+	std::cout << "ABI version: " << mxm.abi_version << std::endl;
+	std::cout << "device ID: " << mxm.device_id << std::endl;
+	std::cout << "capabilitiy flags: " << mxm.flags << std::endl;
+
 	return 0;
 }
